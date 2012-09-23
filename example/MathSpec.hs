20db007c835095c5de741d4bc4adcdf3394854a0
Simon Hengel <sol@typeful.net>
2012-09-23 11:55:14 +0200

    Use `shouldBe` instead of ==

--- example/MathSpec.hs
+++ example/MathSpec.hs
@@ -8,10 +8,10 @@ import Math
 main = hspec $ do
   describe "absolute" $ do
     it "returns the original number when given a positive input" $
-      absolute 1 == 1
+      absolute 1 `shouldBe` 1
 
     it "returns a positive number when given a negative input" $
-      absolute (-1) == 1
+      absolute (-1) `shouldBe` 1
 
     it "returns zero when given zero" $
-      absolute 0 == 0
+      absolute 0 `shouldBe` 0
